c41fa773d1480bf165085c324b6e4b1f65b2bc05
Szilagyi1 <49061899+Szilagyi1@users.noreply.github.com>
2019-04-07 15:04:44 +0300

    Update worker1.js

--- worker1.js
+++ worker1.js
@@ -1,14 +1,24 @@
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-$('#image1').click(function(){
-$(".modal-trigger").click(function(e){
-  e.preventDefault();
-  dataModal = $(this).attr("data-modal");
-  $("#" + dataModal).css({"display":"block"});
-  // $("body").css({"overflow-y": "hidden"}); //Prevent double scrollbar.
-});
+var modal = document.getElementById('myModal');
 
-$(".close-modal, .modal-sandbox").click(function(){
-  $(".modal").css({"display":"none"});
-  // $("body").css({"overflow-y": "auto"}); //Prevent double scrollbar.
-});
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
